253da5d2932c42b699691e8f33c7ee36dc700141
Ævar Arnfjörð Bjarmason <avar@cpan.org>
2010-05-17 23:33:29 +0000

    Import of AVAR/App-QuoteCC-0.01 from CPAN.
    gitpan-cpan-distribution: App-QuoteCC
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         AVAR/App-QuoteCC-0.01.tar.gz
    gitpan-cpan-author:       AVAR
    gitpan-cpan-maturity:     released

---
+++ lib/App/QuoteCC/Format/Fortune.pm
@@ -0,0 +1,40 @@
+package App::QuoteCC::Format::Fortune;
+BEGIN {
+  $App::QuoteCC::Format::Fortune::VERSION = '0.01';
+}
+
+use perl5i::latest;
+use Moose;
+use namespace::clean -except => 'meta';
+
+with qw/ App::QuoteCC::Role::Format /;
+
+sub quotes {
+    my ($self) = @_;
+    my $handle = $self->file_handle;
+
+    my $content = join '', <$handle>;
+    my @quotes = split /\n%\n/, $content;
+    return \@quotes;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+=encoding utf8
+
+=head1 NAME
+
+App::QuoteCC::Format::Fortune - Read quotes from a L<fortune(1)> file
+
+=head1 AUTHOR
+
+E<AElig>var ArnfjE<ouml>rE<eth> Bjarmason <avar@cpan.org>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 E<AElig>var ArnfjE<ouml>rE<eth> Bjarmason <avar@cpan.org>
+
+This program is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
